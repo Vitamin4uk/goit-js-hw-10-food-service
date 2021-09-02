e7375873625c439a68a076e8d76b59d8fc9422b6
Viktor Mostovyi <mvvfreelance@gmai.com>
2021-09-02 14:04:21 +0300

    add src

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-import './sass/main.scss';
+import menuCardsTpl from './templates/menu-cards.hbs';
+import menu from './js/menu.json';
+import './css/style.css';
+
+const refs = {
+    menu: document.querySelector('.js-menu'),
+    checkbox: document.querySelector('#theme-switch-toggle'),
+    body: document.body,
+};
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const STORAGE_KEY = 'theme';
+
+const theme = localStorage.getItem(STORAGE_KEY);
+setTheme(theme);
+
+refs.menu.insertAdjacentHTML('beforeend', menuCardsTpl(menu));
+refs.checkbox.addEventListener('change', onCheckboxChange);
+
+function onCheckboxChange (e) {
+    const theme = e.target.checked ? Theme.DARK : Theme.LIGHT;
+    localStorage.setItem(STORAGE_KEY, theme);
+    refs.body.className = theme;
+}
+
+function setTheme(theme) {
+    if (theme) {
+       refs.body.className = theme;
+       if(theme === Theme.DARK) {
+           refs.checkbox.checked = true;
+       }
+    }
+}
